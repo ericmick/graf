43540efdd486e46a0ac3d7396c66502d2abed12e
Eric Mickelsen <mickelsen.eric@gmail.com>
2015-12-20 16:37:55 -0800

    added cursors and behaviors

--- graph.js
+++ graph.js
@@ -246,112 +246,47 @@ function Decelerate(x0, v0, t0, t1) {
 
 //CanvasGraphView
 //A graph display on a canvas
-function CanvasGraphView(graphViewModel, canvas) {
+function CanvasGraphView(graphViewModel, canvas, behaviors) {
 	this.gvm = graphViewModel;
 	this.c = canvas;
 	this.ctx = canvas.getContext("2d");
 	var view = this;
-	var easeTime = 100;
-	var coastTime = 400;
 	this.selection = null;
 	this.textChanged = [];//event
-	canvas.onmousedown = function(event) {
-		if (view.motion !== null) {//arrest an ongoing motion
-			var now = new Date().getTime();
-			var v = [view.motion[0].v(now), view.motion[1].v(now)];
-			var x = [view.motion[0].x(now), view.motion[1].x(now)];
-			view.motion = [new Ease(x[0], x[0], v[0], now, now + easeTime),
-				new Ease(x[1], x[1], v[1], now, now + easeTime)];
-		}
-		var m0 = [event.clientX, event.clientY];
-		var v0 = view.pickVertex(m0);
-		if (v0 === null) {//pan
-			var pan0 = [view.pan[0], view.pan[1]];
-			view.c.onmousemove = function(event) {
-				var now = new Date().getTime();
-				var m1 = [event.clientX, event.clientY];
-				var d = [(m1[0]-m0[0]), (m1[1]-m0[1])];
-				if (view.motion !== null) {
-					var v = [view.motion[0].v(now), view.motion[1].v(now)];
-					var x = [view.motion[0].x(now), view.motion[1].x(now)];
-					view.motion = [new Ease(x[0], pan0[0]-d[0], v[0], now, now + easeTime),
-						new Ease(x[1], pan0[1]-d[1], v[1], now, now + easeTime)];
-				}
-				else {
-					view.motion = [new Ease(view.pan[0], pan0[0]-d[0], 0, now, now + easeTime),
-						new Ease(view.pan[1], pan0[1]-d[1], 0, now, now + easeTime)];
-					view.animate();
-				}
-			};
-			view.c.onmouseup = function() {
-				view.c.onmousemove = null;
-				if (view.motion !== null) {
-					var now = new Date().getTime();
-					var v = [view.motion[0].v(now), view.motion[1].v(now)];
-					var x = [view.motion[0].x(now), view.motion[1].x(now)];
-					view.motion = [new Decelerate(x[0], v[0], now, now + coastTime),
-						new Decelerate(x[1], v[1], now, now + coastTime)];
-				}
-			};
-		}
-		else {//drag vertex
-			view.c.onmouseup = function(event) {
-				var m1 = [event.clientX, event.clientY];
-				var v1 = view.pickVertex(m1);
-                var a, b;
-				if (v1 !== null && v0 != v1) {//draw edge
-					view.gvm.g.addEdge([v0, v1]);
-				}
-				else if (v1 === null && event.ctrlKey) {//draw edge and new vertex
-					view.gvm.g.addVertex("");
-					a = view.untransform(m1);
-					view.gvm.v[view.gvm.v.length - 1].p[0] = a[0];
-					view.gvm.v[view.gvm.v.length - 1].p[1] = a[1];
-					view.gvm.g.addEdge([v0, view.gvm.g.v.length - 1]);
-				}
-				else if (v1 === null || v0 === v1){//move vertex
-					a = view.untransform(m0);
-					b = view.untransform(m1);
-					view.gvm.v[v0].p[0] += b[0] - a[0];
-					view.gvm.v[v0].p[1] += b[1] - a[1];
-					if (view.motion === null)
-						view.render();
-				}
-			};
-		}
-		return false;
-	};
-	canvas.onclick = function(event) {
-		var m = [event.clientX, event.clientY];
-		var v = view.pickVertex(m);
-		view.selection = v;
-		view.notify(view.textChanged, view.getText());
-		view.render();
-	};
-	canvas.onmouseout = function() {
-		view.c.onmousemove = null;
-	};
-	canvas.onmousewheel = function(event) {
-		view.zoom(event.wheelDelta/1000);
-		if (view.motion === null)
-			view.render();
-		if (event.preventDefault)
-			event.preventDefault();
-		event.returnValue = false;
-	};
 	this.gvm.graphChanged.push(function () {
 		view.render();
 	});
 	this.render();
+	this.behaviors = behaviors || [];
+	this.handleEvents();
 }
 CanvasGraphView.prototype = {
 	scale: 100,
 	pan: [0, 0],
 	motion: null,
-	maxFPS: 40,
+	maxFPS: 30,
 	maxScale: 1450,
 	minScale: 45,
-    animate: function() {
+	handleEvents: function() {
+		var view = this;
+		var events = ['onmousedown', 'onmousemove', 'onmouseout', 'onmouseup', 'onmousewheel'];
+		for (var i = 0; i < events.length; i++) {
+			(function() {
+				var eventName = events[i];
+				view.c[eventName] = function(event) {
+					for (var j = 0; j < view.behaviors.length; j++) {
+						if (typeof view.behaviors[j][eventName] == 'function') {
+							if (view.behaviors[j][eventName](event, view)) {
+								//if behaviors' event handlers return something truthy, consider the event consumed.
+								return;
+							}
+						}
+					}
+				};
+			})();
+		}
+	},
+	animate: function() {
 		var t = new Date().getTime();
 		if (this.motion !== null) {
 			if (t > this.motion[0].end && t > this.motion[1].end) {
@@ -360,13 +295,13 @@ CanvasGraphView.prototype = {
 				this.render();
 				return;
 			}
-			this.pan = [this.motion[0].x(t), this.motion[1].x(t)];			
+			this.pan = [this.motion[0].x(t), this.motion[1].x(t)];
 			this.render();
 			var frameDuration = new Date().getTime() - t;
 			var view = this;
 			setTimeout(function(){view.animate();}, frameDuration < 1000/this.maxFPS ? 1000/this.maxFPS - frameDuration : 1);
 		}
-    },
+	},
 	zoom: function(f) {
 		var oldScale = this.scale;
 		this.scale += f * oldScale;
@@ -375,13 +310,13 @@ CanvasGraphView.prototype = {
 		this.pan[0] += (this.scale - oldScale) / oldScale * this.pan[0];
 		this.pan[1] += (this.scale - oldScale) / oldScale * this.pan[1];
 	},
-	transform: function (v) {
+	transform: function(v) {
 		return [v[0] * this.scale - this.pan[0] + this.c.width/2, v[1] * this.scale - this.pan[1] + this.c.height/2];
 	},
-	untransform: function (v) {
+	untransform: function(v) {
 		return [(v[0] + this.pan[0] - this.c.width/2) / this.scale, (v[1] + this.pan[1] - this.c.height/2) / this.scale];
 	},
-	pickVertex: function (p) {
+	pickVertex: function(p) {
 		p = this.untransform(p);
 		for (var i = 0; i < this.gvm.v.length; i++) {
 			var v = this.gvm.v[i];
@@ -389,8 +324,9 @@ CanvasGraphView.prototype = {
 			if (d[0] < v.width / 2 && d[1] <= v.height / 2) {//bounding box
 				var sqrOfDistance = d[0] * d[0] + d[1] * d[1];//from center
 				var angle = Math.atan2(d[0] / v.width, d[1] / v.height);
-				if (sqrOfDistance < Math.pow(Math.sin(angle) * v.width / 2, 2) + Math.pow(Math.cos(angle) * v.height / 2, 2))
+				if (sqrOfDistance < Math.pow(Math.sin(angle) * v.width / 2, 2) + Math.pow(Math.cos(angle) * v.height / 2, 2)) {
 					return i;
+				}
 			}
 		}
 		return null;
@@ -476,5 +412,210 @@ CanvasGraphView.prototype = {
 		for (var j = 0; j < eventHandlers.length; j++) {
 			eventHandlers[j](eventArgs);
 		}
+	},
+	addBehavior: function(behavior) {
+		this.behaviors.push(behavior);
+	},
+	removeBehavior: function(behavior) {
+		for(var i = 0; i < this.behaviors.length; i++) {
+			if (this.behaviors[i] == behavior) {
+				this.behaviors.splice(i, 1);
+				return;
+			}
+		}
+	},
+	setCursor: function(cursor) {
+		//these cursors rely on a stylesheet with classes :-(
+		var cssClassCursors = ['grab', 'grabbing'];
+		for (var i = 0; i < cssClassCursors.length; i++) {
+			this.c.className = this.c.className.replace(new RegExp('\\b' + cssClassCursors[i] + '\\b', 'g'), '');
+		}
+		switch (cursor) {
+			case 'grab':
+			case 'grabbing':
+				this.c.className += ' ' + cursor;
+				this.c.style.cursor = '';
+			default:
+				this.c.style.cursor = cursor;
+		}
+	}
+};
+
+function Behavior(modifiers) {
+	this.modifiers = modifiers || [];
+}
+Behavior.prototype = {
+	applies: function(event) {
+		for(var i = 0; i < this.modifiers.length; i++) {
+			if (!this.modifiers[i](event)) {
+				return false;
+			}
+		}
+		return true;
+	},
+	modify: function(prop) {
+		this.modifiers.push(function(event) {
+			return event[prop];
+		});
+		return this;
+	},
+	ctrl: function() {
+		return this.modify('ctrlKey');
+	},
+	shift: function() {
+		return this.modify('shiftKey');
+	},
+	alt: function() {
+		return this.modify('altKey');
+	},
+	meta: function() {
+		return this.modify('metaKey');
+	}
+};
+
+function Panning(modifiers, easeTime, coastTime) {
+	this.easeTime = (typeof easeTime != 'undefined') ? easeTime : 100;
+	this.coastTime = (typeof coastTime != 'undefined') ? coastTime : 400;
+	Behavior.call(this, modifiers);
+}
+Panning.prototype = Object.create(Behavior.prototype);
+Panning.prototype.constructor = Panning;
+Panning.prototype.onmousedown = function(event, view) {
+	if (view.motion !== null) {
+		//arrest an ongoing motion
+		var now = new Date().getTime();
+		var v = [view.motion[0].v(now), view.motion[1].v(now)];
+		var x = [view.motion[0].x(now), view.motion[1].x(now)];
+		view.motion = [new Ease(x[0], x[0], v[0], now, now + this.easeTime),
+			new Ease(x[1], x[1], v[1], now, now + this.easeTime)];
+	}
+	var m0 = [event.clientX, event.clientY];
+	//pan
+	var pan0 = [view.pan[0], view.pan[1]];
+	view.setCursor('grabbing');
+	this.onmousemove = function(event) {
+		var now = new Date().getTime();
+		var m1 = [event.clientX, event.clientY];
+		var d = [(m1[0]-m0[0]), (m1[1]-m0[1])];
+		if (view.motion !== null) {
+			var v = [view.motion[0].v(now), view.motion[1].v(now)];
+			var x = [view.motion[0].x(now), view.motion[1].x(now)];
+			view.motion = [new Ease(x[0], pan0[0]-d[0], v[0], now, now + this.easeTime),
+				new Ease(x[1], pan0[1]-d[1], v[1], now, now + this.easeTime)];
+		} else {
+			view.motion = [new Ease(view.pan[0], pan0[0]-d[0], 0, now, now + this.easeTime),
+				new Ease(view.pan[1], pan0[1]-d[1], 0, now, now + this.easeTime)];
+			view.animate();
+		}
+	};
+	this.onmouseup = function() {
+		view.setCursor('grab');
+		this.onmousemove = Panning.prototype.onmousemove;
+		if (view.motion !== null) {
+			var now = new Date().getTime();
+			var v = [view.motion[0].v(now), view.motion[1].v(now)];
+			var x = [view.motion[0].x(now), view.motion[1].x(now)];
+			view.motion = [new Decelerate(x[0], v[0], now, now + this.coastTime),
+				new Decelerate(x[1], v[1], now, now + this.coastTime)];
+		}
+	};
+	return false;
+};
+Panning.prototype.onclick = function(event, view) {
+	var m = [event.clientX, event.clientY];
+	var v = view.pickVertex(m);
+	view.selection = v;
+	view.notify(view.textChanged, view.getText());
+	view.render();
+};
+Panning.prototype.onmousemove = function(event, view) {
+	var m0 = [event.clientX, event.clientY];
+	var v0 = view.pickVertex(m0);
+	if (v0 === null) {
+		view.setCursor('grab');
+	}
+};
+Panning.prototype.onmousewheel = function(event, view) {
+	view.zoom(event.wheelDelta/1000);
+	if (view.motion === null)
+		view.render();
+	if (event.preventDefault)
+		event.preventDefault();
+	event.returnValue = false;
+};
+Panning.prototype.onmouseout = function(event, view) {
+	if (typeof this.onmouseup == 'function') {
+		this.onmouseup(event, view);
+	}
+};
+
+function Moving(modifiers) {
+	Behavior.call(this, modifiers);
+}
+Moving.prototype = Object.create(Behavior.prototype);
+Moving.prototype.constructor = Moving;
+Moving.prototype.onmousemove = function(event, view) {
+	var m0 = [event.clientX, event.clientY];
+	var v0 = view.pickVertex(m0);
+	if (v0 !== null) {
+		view.setCursor('move');
+		//consider this event consumed
+		return true;
+	}
+};
+Moving.prototype.onmousedown = function(event, view) {
+	var m0 = [event.clientX, event.clientY];
+	var v0 = view.pickVertex(m0);
+	if (v0 !== null) {
+		//drag vertex
+		view.setCursor('move');
+		this.onmousemove = function(event) {
+			var m1 = [event.clientX, event.clientY];
+			var v1 = view.pickVertex(m1);
+			var a, b;
+			if (v1 !== null && v0 != v1) {
+				//draw edge
+				view.setCursor('alias');
+			} else if (v1 === null || v0 === v1) {
+				//move vertex
+				view.setCursor('move');
+			}
+			//consider this event consumed
+			return true;
+		};
+		this.onmouseup = function(event) {
+			this.onmousemove = Moving.prototype.onmousemove;
+			this.onmouseup = null;
+			var m1 = [event.clientX, event.clientY];
+			var v1 = view.pickVertex(m1);
+			var a, b;
+			if (v1 !== null && v0 != v1) {
+				//draw edge
+				view.gvm.g.addEdge([v0, v1]);
+			} else if (v1 === null && event.ctrlKey) {
+				//draw edge and new vertex
+				view.gvm.g.addVertex('');
+				a = view.untransform(m1);
+				view.gvm.v[view.gvm.v.length - 1].p[0] = a[0];
+				view.gvm.v[view.gvm.v.length - 1].p[1] = a[1];
+				view.gvm.g.addEdge([v0, view.gvm.g.v.length - 1]);
+			} else if (v1 === null || v0 === v1) {
+				//move vertex
+				a = view.untransform(m0);
+				b = view.untransform(m1);
+				view.gvm.v[v0].p[0] += b[0] - a[0];
+				view.gvm.v[v0].p[1] += b[1] - a[1];
+				if (view.motion === null)
+					view.render();
+			}
+		};
+		//consider this event consumed
+		return true;
+	}
+	return false;
+};
+Moving.prototype.onmouseout = function(event, view) {
+	if (typeof this.onmouseup == 'function') {
+		this.onmouseup(event, view);
 	}
 };
